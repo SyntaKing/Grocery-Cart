a2c766c14114a441a5b16a4bfe12e7500821568a
Ashish Daniel <ashish.daniel@outlook.com>
2025-07-11 02:40:28 +0530

    Alter main menu, update README.md

--- README.md
+++ README.md
@@ -4,7 +4,3 @@ This is a very simple practice project that I'm making to practice python.
 Feel free to suggest features that I can add to this project.
 
 ## Features
-
-- View Cart
-- Add Item
-- Remove Item
--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import os
 
-#Item Declaration
+#Variable Delaration
 inventory_fruits = {"Apple" : 69, "Banana" : 8.75}
-cart = []
+
+items = []
+quantity = []
+cart = [items, quantity]
+
+total = 0
 
 # Logic execution
 while True:
-    os.system('cls')
-    #Main Menu
     while True:
-        print("-------Grocery Cart-------")
-        print("\n1. View Cart\n2.Add Item\n3.Remove Item\nQ. Quit program\n__________________________")
+        #Main Menu
+        os.system('cls')
+        print("-----------Grocery Cart-----------")
+        if len(items) == 0:
+            print(f"\n\n          Cart is empty!\n\n\nTotal amount: ₹{total}")
+        print("\n1. Add Item\n2. Remove Item\n3. Checkout\n\nA. Admin Panel\nQ. Quit\n__________________________________")
         choice = input()
+        while (choice == '') or ((choice.lower() != 'q' and choice.lower() != 'a') and (choice> '3' or choice< '1')) or len(choice) > 1:
+            os.system('cls')
+            print("-----------Grocery Cart-----------")
+            if len(items) == 0:
+                print(f"\n\n          Cart is empty!\n\n\nTotal amount: ₹{total}")
+            print("\n1. Add Item\n2. Remove Item\n3. Checkout\n\nA. Admin Panel\nQ. Quit\n\nINVALID CHOICE!\n__________________________________")
+            choice = input()
+        
+        #Quit
         if choice.lower() == 'q':
             break
-    
+
     #Exit confirmation
     os.system('cls')
-    print("Press 'Q' again to confirm:\n__________________________")
+    print("-----------Grocery Cart-----------")
+    print("\nPress 'Q' again to confirm\nOR\nPress any other key to go back:\n__________________________________")
     choice = input()
     if choice.lower() == 'q':
         os.system('cls')
