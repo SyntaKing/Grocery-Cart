4d8c9f1d0c5c168d4523aa905eba448dc7ae7abb
Ashish Daniel <ashish.daniel@outlook.com>
2025-07-11 07:47:45 +0530

    Add feature: Add Item

--- main.py
+++ main.py
@@ -1,40 +1,141 @@
 import os
 
 #Variable Delaration
-inventory_fruits = {"Apple" : 69, "Banana" : 8.75}
+price_list = {"Apple [1 pc]" : 69, "Banana [1 pc]" : 8.75, "Mango [1 pc]" : 27, "Potato" : 23, "Cucumber" : 44}
 
-items = []
+fruits = ["Apple [1 pc]", "Banana [1 pc]", "Mango [1 pc]"]
+vegetables = ["Potato [1 KG]", "Cucumber [500 GM]"]
+
+cart_items = []
 quantity = []
-cart = [items, quantity]
+cart = [cart_items, quantity]
 
-total = 0
+total = 0.0
+
+def viewCart():
+    total = 0.0
+    print("")
+    for i in range(len(cart_items)):
+        print(f"{cart_items[i]}  x  {quantity[i]}      ₹{float(price_list[cart_items[i]])*float(quantity[i])}")
+        print("-------------------")
+        total += float(price_list[cart_items[i]])*float(quantity[i])
+    print(f"\nCart total:               ₹{total}")
+
+def addToCart (list_name, item: int, item_quantity: int): 
+    if list_name == 'fruits':
+        cart_items.append(fruits[int(item)-1])
+        quantity.append(item_quantity)
 
 # Logic execution
 while True:
     while True:
+        choice_main_menu = None
+        choice_submenu_1 = None
+        choice_submenu_2 = None
+        item_quantity = None
+        list_name = None
+
         #Main Menu
         os.system('cls')
         print("-----------Grocery Cart-----------")
-        if len(items) == 0:
-            print(f"\n\n          Cart is empty!\n\n\nTotal amount: ₹{total}")
-        print("\n1. Add Item\n2. Remove Item\n3. Checkout\n\nA. Admin Panel\nQ. Quit\n__________________________________")
-        choice = input()
-        while (choice == '') or ((choice.lower() != 'q' and choice.lower() != 'a') and (choice> '3' or choice< '1')) or len(choice) > 1:
+
+        #View Cart
+        if len(cart_items) == 0:
+            print(f"\n\n          Cart is empty!\n\n\nCart total:               ₹{total}")
+        else:
+            viewCart()
+
+        print("\n1. Add Item\n2. Remove Item\n3. Checkout\n\nA. Admin Panel\nQ. Quit")
+        print("__________________________________")
+        choice_main_menu = input()
+        while (choice_main_menu == '') or ((choice_main_menu.lower() != 'q' and choice_main_menu.lower() != 'a') and (choice_main_menu> '3' or choice_main_menu< '1')) or len(choice_main_menu) > 1:
             os.system('cls')
             print("-----------Grocery Cart-----------")
-            if len(items) == 0:
-                print(f"\n\n          Cart is empty!\n\n\nTotal amount: ₹{total}")
-            print("\n1. Add Item\n2. Remove Item\n3. Checkout\n\nA. Admin Panel\nQ. Quit\n\nINVALID CHOICE!\n__________________________________")
-            choice = input()
+            #View Cart
+            if len(cart_items) == 0:
+                print(f"\n\n          Cart is empty!\n\n\nCart total:               ₹{total}")
+            else:
+                viewCart()
+            print("\n1. Add Item\n2. Remove Item\n3. Checkout\n\nA. Admin Panel\nQ. Quit\n\nINVALID INPUT!")
+            print("__________________________________")
+            choice_main_menu = input()
         
         #Quit
-        if choice.lower() == 'q':
+        if choice_main_menu.lower() == 'q':
             break
 
+        #Add Item Menu
+        while choice_main_menu == '1':
+            os.system('cls')
+            print("-----------Grocery Cart-----------")
+            print("\n             ADD ITEM")
+            print("\n1. Add Fruits\n2. Add Vegetables\n\n-  Previous menu")
+            print("__________________________________")
+            choice_submenu_1 = input()
+            while (choice_submenu_1 != '-') and (choice_submenu_1 < '1' or choice_submenu_1 > '2'):
+                os.system('cls')
+                print("-----------Grocery Cart-----------")
+                print("\n             ADD ITEM")
+                print("\n1. Add Fruits\n2. Add Vegetables\n\n-  Previous menu")
+                print("\nINVALID INPUT!")
+                print("__________________________________")
+                choice_submenu_1 = input()
+
+            if choice_submenu_1 == '-':
+                break
+
+            while choice_submenu_1 == '1':
+                os.system('cls')
+                list_name = 'fruits'
+                print("-----------Grocery Cart-----------")
+                print("\n             ADD ITEM\n")
+                serial = 1
+                for i in fruits:
+                    print(f"{serial}. {i}")
+                    serial += 1
+                serial -= 1
+                print("\n\n-  Previous menu")
+                print("__________________________________")
+                choice_submenu_2 = input()
+                while ((choice_submenu_2.isalpha == True) or (choice_submenu_2 < '1' or choice_submenu_2 > str(serial)) or len(choice_submenu_2) != len(str(serial))) and (choice_submenu_2 != '-'):
+                    os.system('cls')
+                    print("-----------Grocery Cart-----------")
+                    print("\n             ADD ITEM\n")
+                    serial = 1
+                    for i in fruits:
+                        print(f"{serial}. {i}")
+                        serial += 1
+                    serial -= 1
+                    print("\n\n-  Previous menu")
+                    print("\nINVALID INPUT")
+                    print("__________________________________")
+                    choice_submenu_2 = input()
+
+                if choice_submenu_2 == '-':
+                    break
+
+                else:
+                    os.system('cls')
+                    print("-----------Grocery Cart-----------")
+                    print("\n             ADD ITEM\n")
+                    print("\nEnter quantity")
+                    print("__________________________________")
+                    item_quantity = input()
+                    while item_quantity.isdigit() != True:
+                        os.system('cls')
+                        print("-----------Grocery Cart-----------")
+                        print("\n             ADD ITEM\n")
+                        print("\nEnter quantity")
+                        print("\nINVALID INPUT")
+                        print("__________________________________")
+                        item_quantity = input()
+                    addToCart(list_name, choice_submenu_2, item_quantity)
+                    
     #Exit confirmation
     os.system('cls')
     print("-----------Grocery Cart-----------")
-    print("\nPress 'Q' again to confirm\nOR\nPress any other key to go back:\n__________________________________")
+    print("\nPress 'Q' again to confirm\nOR\nPress any other key to go back:")
+    print("__________________________________")
     choice = input()
     if choice.lower() == 'q':
         os.system('cls')
